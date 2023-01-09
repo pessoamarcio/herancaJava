bcb7026104fb9151197802ad188acf7c34e96985
pessoamarcio <103587377+pessoamarcio@users.noreply.github.com>
2023-01-09 13:52:27 -0300

    Update ControleLoja.java

--- ControleLoja.java
+++ ControleLoja.java
@@ -1,5 +1,16 @@
 package estoqueLoja;
 
+/*2-Crie uma um programa para trabalhar com estoque de uma loja, o programa deverá 
+ * trabalhar com Collection do tipo List do Java para manipular os dados desse estoque, 
+ * o programa deverá atender as seguintes funcionalidades:
+ *
+ * Armazenar dados da List;
+ * Remover dados da list;
+ * Atualizar dados da list.
+ * Apresentar todos os dados da list.
+*/
+
+
 import java.util.ArrayList;
 import java.util.List;
 
